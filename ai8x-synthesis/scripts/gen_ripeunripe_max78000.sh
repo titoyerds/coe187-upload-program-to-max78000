1b4668d08caaba64c28cfb3575044f4a8e76fad3
Andrey Fritz Solijon <161728738+titoyerds@users.noreply.github.com>
2026-01-13 17:26:10 +0800

    Add HWC config and update ripe-unripe script

--- ai8x-synthesis/scripts/gen_ripeunripe_max78000.sh
+++ ai8x-synthesis/scripts/gen_ripeunripe_max78000.sh
@@ -3,4 +3,4 @@ DEVICE="MAX78000"
 TARGET="sdk/Examples/$DEVICE/CNN"
 COMMON_ARGS="--device $DEVICE --timer 0 --display-checkpoint --verbose"
 
-python ai8xize.py --test-dir sdk/Examples/MAX78000/CNN --prefix ripe-unripe --checkpoint-file trained/best-quantized.pth.tar --config-file networks/ripe-unripe-hwc-no-fifo.yaml --softmax --device MAX78000 --timer 0 --display-checkpoint --verbose
+python ai8xize.py --test-dir sdk/Examples/MAX78000/CNN --prefix ripe-unripe --checkpoint-file trained/best-quantized.pth.tar --config-file networks/ripe-unripe-hwc.yaml --fifo --softmax --device MAX78000 --timer 0 --display-checkpoint --verbose
